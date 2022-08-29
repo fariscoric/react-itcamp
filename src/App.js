6728e0ebaeb9f7811df4e3046974d0b8e43fd2a3
fariscoric <faris.coric@gmail.com>
2022-08-29 21:58:25 +0800

    Axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import './App.css';
-import React from 'react';
-import { useState } from 'react';
-import CryptoForm from './components/CryptoForm/CryptoForm'
-import PostsList from './components/Posts2/PostsList'
+import axios from 'axios'
+import React, { useState ,useEffect } from 'react'
+import './App.css'
+import Posts from './components/Posts/Posts'
 
-
+const BASE_URL = "https://api.quotable.io"
 export default function App() {
+  const [authors , setAuthors] = useState([])
+  const [loading , setLoading] = useState(false)
+  const [pagination, setPagination] = useState({})
+  const [page ,setPage] = useState(1)
 
-    return (
-      <div className='card-container'>
-        <PostsList />
-      </div>
+  function getAuthors(page) {
+    axios.get(`${BASE_URL}/authors?sortBy=quoteCount&page=${page}`)
+    .then((res) => {
+      console.log(res.data.results)
+      setPagination({
+        page: res.data.page,
+        lastPage: res.data.totalPages
+      })
+      setAuthors(res.data.results)})
+
+  }
+
+  useEffect(() => {
+     getAuthors(page);
+  }, [page]);
+
+  return (
+    <div className='card-container'>
+      
+      {authors.length > 0 ? (
+        <div>
+          <div>{pagination.page} / {pagination.lastPage}</div>
+          <button onClick={() => setPage((prev) => prev + 1)}>Next</button>
+        {authors.map((author) => (
+          <div key={author._id}>
+          <h3>{author.name}</h3>
+          <h4>{author.description}</h4>
+          </div>
+        ))}
+        </div>
+      ) : ( <p>Loading...</p>
+        )}
+    </div>
   )
-} 
+}
