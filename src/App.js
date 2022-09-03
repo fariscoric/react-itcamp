cd84ced5dcd67e71f0818dbfcdc12e9da2287a85
fariscoric <faris.coric@gmail.com>
2022-09-04 00:00:15 +0800

    AxiosDelete

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { useState ,useEffect } from 'react'
 import './App.css'
 import PersonList from './components/PersonList/PersonList'
 import PersonAdd from './components/PersonList/PersonAdd'
+import PersonDelete from './components/PersonList/PersonDelete'
 
 export default function App() {
   return (
     <div className='card-container'>
       <PersonAdd/>
       <PersonList/>
+      <PersonDelete/>
     </div>
   )
 }
