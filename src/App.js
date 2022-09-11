832bf7dedb19a306f166a224a47c3b641ee9674a
fariscoric <faris.coric@gmail.com>
2022-09-11 23:23:13 +0800

    Route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import axios from 'axios'
 import React, { useState ,useEffect } from 'react'
 import './App.css'
 import NextUI from './components/NextUI/NextUI'
+import { Routes, Route, Link } from 'react-router-dom';
 
 
 export default function App() {
   return (
     <div className='card-container'>
-      <NextUI />
+      <h1>Welcome to React Router</h1>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>}/>
+        <Route path="about" element={<h1> About Page</h1>}/>
+      </Routes>
     </div>
   )
 }
